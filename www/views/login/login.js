f7fc4c1e47b2318555aac15304284346494d05d1
danding <1291248710@qq.com>
2016-11-12 08:19:03 +0800

    xx

--- www/views/login/login.js
+++ www/views/login/login.js
@@ -416,5 +416,30 @@ angular.module('starter')
           $state.go('passwordForget');
       }
 
+        $scope.select={
+        };
+        $scope.selectChanged=function () {
+            var item=$scope.select.item;
+            console.log('...');
+        }
+
+        $scope.optData = [{
+            id: 10001,
+            MainCategory: '男',
+            ProductName: '水洗T恤',
+            ProductColor: '白'
+        },{
+            id: 10002,
+            MainCategory: '女',
+            ProductName: '圓領短袖',
+            ProductColor: '黃'
+        },{
+            id: 10003,
+            MainCategory: '女',
+            ProductName: '圓領短袖',
+            ProductColor: '黃'
+        }];
+
+
 
     });
