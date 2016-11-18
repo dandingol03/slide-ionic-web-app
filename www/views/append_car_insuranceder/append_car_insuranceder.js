8c2696bb7f15cac22c75acc15bb0c234bb47e391
danding <1291248710@qq.com>
2016-11-18 11:45:57 +0800

    xx

--- www/views/append_car_insuranceder/append_car_insuranceder.js
+++ www/views/append_car_insuranceder/append_car_insuranceder.js
@@ -5,7 +5,8 @@ angular.module('starter')
 
   .controller('appendCarInsurancederController',function($scope,$state,$http, $location,
                                                 $rootScope,$ionicActionSheet,$cordovaCamera,$cordovaImagePicker,
-                                                $ionicModal,Proxy,$stateParams,$cordovaFileTransfer){
+                                                $ionicModal,Proxy,$stateParams,$cordovaFileTransfer,
+                                                $q){
 
     $scope.go_back=function(){
       window.history.back();
@@ -178,47 +179,73 @@ angular.module('starter')
     //提交车险意向
     $scope.confirm=function(){
 
-      alert('personId='+$scope.insuranceder.personId);
-
-      $http({
-        method: "POST",
-        url: Proxy.local()+"/svr/request",
-        headers: {
-          'Authorization': "Bearer " + $rootScope.access_token
-        },
-        data:
+      //已有照片
+        if($scope.car_insurance.insuranceder.perIdCard_img!==undefined&&$scope.car_insurance.insuranceder.perIdCard_img!==null)
         {
-          request:'generateCarInsuranceOrder',
-          info:
-          {
-            products:$scope.info.products,
-            companys:$scope.info.companys,
-            carId:$scope.info.carId,
-            insurancederId:$scope.insuranceder.personId
-          }
-        }
-      }).then(function(res) {
-        var json=res.data;
-        var orderId=json.data;
-        if(orderId!==undefined&&orderId!==null)
-        {
+            //提交照片
+            $scope.upload('createRelativePerson',$scope.car_insurance.insuranceder,'perIdCard_img').then(function(json) {
+                if(json.re==1) {
+                    $http({
+                        method: "POST",
+                        url: Proxy.local()+"/svr/request",
+                        headers: {
+                            'Authorization': "Bearer " + $rootScope.access_token
+                        },
+                        data:
+                            {
+                                request:'generateCarInsuranceOrder',
+                                info:
+                                    {
+                                        products:$scope.info.products,
+                                        companys:$scope.info.companys,
+                                        carId:$scope.info.carId,
+                                        insurancederId:$scope.insuranceder.personId
+                                    }
+                            }
+                    }).then(function(res) {
+                        var json=res.data;
+                        var orderId=json.data;
+                        if(orderId!==undefined&&orderId!==null)
+                        {
 
-          alert('订单以创建,请等待报价');
+                            alert('订单已创建,请等待报价');
 
-          $state.go('car_orders');
+                            $state.go('car_orders');
+                        }
+                    }).catch(function(err) {
+                        var str='';
+                        for(var field in err)
+                            str+='field'+field+'\r\n'
+                                +err[field];
+                        console.error('error=\r\n' + str);
+                    });
+                }
+            })
+        }else{
+
+            var confirmPopup = $ionicPopup.confirm({
+                title: '您还未上传身份证',
+                template: '是否现在进行拍照'
+            });
+            confirmPopup.then(function(res) {
+                if(res) {
+                    $scope.addAttachment($scope.car_insurance.insuranceder,'perIdCard1_img');
+                } else {}
+            });
         }
-      }).catch(function(err) {
-        var str='';
-        for(var field in err)
-          str+='field'+field+'\r\n'
-            +err[field];
-        console.error('error=\r\n' + str);
-      });
+
+
     }
 
 
 //提交统一函数
     $scope.upload=function(cmd,item,field){
+
+        var deferred=$q.defer();
+        var server='';
+
+        var perIdAttachId1 = null;
+        var perIdAttachId2 = null;
 
       var personId=null;
       $http({
@@ -237,140 +264,135 @@ angular.module('starter')
 
           var json =res.data;
           alert(json.re);
-          if(json.re==1){
+          if(json.re==1) {
 
-            personId=json.data.personId;
+              personId = json.data.personId;
+              $scope.insuranceder.personId = personId;
 
-            $scope.insuranceder.personId=personId;
-
-            alert('personid='+personId);
-            alert('personid='+$scope.insuranceder.personId);
-            var suffix='';
-            var imageType='perIdCard';
-            alert('path='+$scope.car_insurance.insuranceder.perIdCard1_img);
-            if($scope.car_insurance.insuranceder.perIdCard1_img.indexOf('.jpg')!=-1)
-              suffix='jpg';
-            else if($scope.car_insurance.insuranceder.perIdCard1_img.indexOf('.png')!=-1)
-              suffix='png';
-            else{}
-            var server=Proxy.local()+'/svr/request?request=uploadPhoto' +
-              '&imageType='+imageType+'&suffix='+suffix+
-              '&filename='+'perIdCard1_img'+'&personId='+personId;
-            var options = {
-              fileKey:'file',
-              headers: {
-                'Authorization': "Bearer " + $rootScope.access_token
+              var suffix = '';
+              var imageType = 'perIdCard';
+              if ($scope.car_insurance.insuranceder.perIdCard1_img.indexOf('.jpg') != -1)
+                  suffix = 'jpg';
+              else if ($scope.car_insurance.insuranceder.perIdCard1_img.indexOf('.png') != -1)
+                  suffix = 'png';
+              else {
               }
-            };
-
-            var perIdAttachId1=null;
-            var perIdAttachId2=null;
-
-            $cordovaFileTransfer.upload(server, $scope.car_insurance.insuranceder.perIdCard1_img, options)
-              .then(function(res) {
-                alert('upload perIdCard1 success');
-                for(var field in res) {
-                  alert('field=' + field + '\r\n' + res[field]);
-                }
-                var su=null
-                if($scope.car_insurance.insuranceder.perIdCard1_img.indexOf('.jpg')!=-1)
-                  su='jpg';
-                else if($scope.car_insurance.insuranceder.perIdCard1_img.indexOf('.png')!=-1)
-                  su='png';
-                alert('suffix=' + su);
-                return $http({
-                  method: "POST",
-                  url: Proxy.local()+"/svr/request",
+              server = Proxy.local() + '/svr/request?request=uploadPhoto' +
+                  '&imageType=' + imageType + '&suffix=' + suffix +
+                  '&filename=' + 'perIdCard1_img' + '&personId=' + personId;
+              var options = {
+                  fileKey: 'file',
                   headers: {
-                    'Authorization': "Bearer " + $rootScope.access_token,
-                  },
-                  data:
-                  {
-                    request:'createPhotoAttachment',
-                    info:{
-                      imageType:'perIdCard',
-                      filename:'perIdAttachId1',
-                      suffix:su,
-                      docType:'I1' ,
-                      personId:personId
-                    }
+                      'Authorization': "Bearer " + $rootScope.access_token
                   }
-                });
-              })
-              .then(function(res) {
-                var json=res.data;
-                if(json.re==1) {
-                  perIdAttachId1=json.data;
-                  alert('perIdAttachId1=' + perIdAttachId1);
-                  var su=null;
-                  if($scope.car_insurance.insuranceder.perIdCard2_img.indexOf('.jpg')!=-1)
-                    su='jpg';
-                  else if($scope.car_insurance.insuranceder.perIdCard2_img.indexOf('.png')!=-1)
-                    su='png';
-                  server=Proxy.local()+'/svr/request?request=uploadPhoto' +
-                    '&imageType='+imageType+'&suffix='+su+'&filename='+'perIdAttachId2'+'&personId='+personId;
-                  return  $cordovaFileTransfer.upload(server, $scope.car_insurance.insuranceder.perIdCard2_img, options)
-                    .then(function(res) {
-                      alert('upload perIdCard2 success');
-                      for(var field in res) {
-                        alert('field=' + field + '\r\n' + res[field]);
-                      }
-                      return $http({
-                        method: "POST",
-                        url: Proxy.local()+"/svr/request",
-                        headers: {
-                          'Authorization': "Bearer " + $rootScope.access_token,
-                        },
-                        data:
-                        {
-                          request:'createPhotoAttachment',
-                          info:{
-                            imageType:'perIdCard',
-                            filename:'perIdAttachId2',
-                            suffix:su,
-                            docType:'I1' ,
-                            personId:personId
-                          }
-                        }
-                      });
-                    }) .then(function(res) {
-                      var json=res.data;
-                      if(json.re==1){
-                        perIdAttachId2=json.data;
-                        return $http({
-                          method: "POST",
-                          url: Proxy.local()+"/svr/request",
-                          headers: {
-                            'Authorization': "Bearer " + $rootScope.access_token,
-                          },
-                          data:
-                          {
-                            request:'createInsuranceInfoPersonInfo',
-                            info:{
-                              perIdAttachId1:perIdAttachId1,
-                              perIdAttachId2:perIdAttachId2,
-                              personId:personId
-                            }
-                          }
-                        });
-                      }
-                    })
-                }
+              };
 
-              })
-          }else{}
-
-          alert('...it is back')
+              return $cordovaFileTransfer.upload(server, $scope.car_insurance.insuranceder.perIdCard1_img, options);
+          }else{
+              deferred.reject('command encounter error');
+          }
         }).then(function(res) {
 
+          alert('upload perIdCard1 success');
+          for (var field in res) {
+              alert('field=' + field + '\r\n' + res[field]);
+          }
+          var su = null
+          if ($scope.car_insurance.insuranceder.perIdCard1_img.indexOf('.jpg') != -1)
+              su = 'jpg';
+          else if ($scope.car_insurance.insuranceder.perIdCard1_img.indexOf('.png') != -1)
+              su = 'png';
+          alert('suffix=' + su);
+          return $http({
+              method: "POST",
+              url: Proxy.local() + "/svr/request",
+              headers: {
+                  'Authorization': "Bearer " + $rootScope.access_token,
+              },
+              data: {
+                  request: 'createPhotoAttachment',
+                  info: {
+                      imageType: 'perIdCard',
+                      filename: 'perIdAttachId1',
+                      suffix: su,
+                      docType: 'I1',
+                      personId: personId
+                  }
+              }
+          });
+      }).then(function (res) {
+          var json = res.data;
+          if (json.re == 1) {
+              perIdAttachId1 = json.data;
+              alert('perIdAttachId1=' + perIdAttachId1);
+              var su = null;
+              if ($scope.car_insurance.insuranceder.perIdCard2_img.indexOf('.jpg') != -1)
+                  su = 'jpg';
+              else if ($scope.car_insurance.insuranceder.perIdCard2_img.indexOf('.png') != -1)
+                  su = 'png';
+              server = Proxy.local() + '/svr/request?request=uploadPhoto' +
+                  '&imageType=' + imageType + '&suffix=' + su + '&filename=' + 'perIdAttachId2' + '&personId=' + personId;
+
+              return $cordovaFileTransfer.upload(server, $scope.car_insurance.insuranceder.perIdCard2_img, options);
+          }
+      }).then(function (res) {
+            alert('upload perIdCard2 success');
+            for (var field in res) {
+                alert('field=' + field + '\r\n' + res[field]);
+            }
+
+            return $http({
+                  method: "POST",
+                  url: Proxy.local() + "/svr/request",
+                  headers: {
+                      'Authorization': "Bearer " + $rootScope.access_token,
+                  },
+                  data: {
+                      request: 'createPhotoAttachment',
+                      info: {
+                          imageType: 'perIdCard',
+                          filename: 'perIdAttachId2',
+                          suffix: su,
+                          docType: 'I1',
+                          personId: personId
+                      }
+                  }
+              })
+      }).then(function (res) {
+            var json = res.data;
+            if (json.re == 1) {
+                perIdAttachId2 = json.data;
+                $http({
+                    method: "POST",
+                    url: Proxy.local() + "/svr/request",
+                    headers: {
+                        'Authorization': "Bearer " + $rootScope.access_token,
+                    },
+                    data: {
+                        request: 'createInsuranceInfoPersonInfo',
+                        info: {
+                            perIdAttachId1: perIdAttachId1,
+                            perIdAttachId2: perIdAttachId2,
+                            personId: personId
+                        }
+                    }
+                }).then(function(res) {
+                    var json=res.data;
+                    if(json.re==1) {
+                        deferred.resolve({re: 1, data: '上传照片完成'});
+                    }
+                })
+            }
       })
       .catch(function(err) {
           var str='';
           for(var field in err)
             str+=err[field];
           alert('error=\r\n' + str);
+          deferred.reject(str);
         });
 
+      return deferred.promise;
     }
 
 
