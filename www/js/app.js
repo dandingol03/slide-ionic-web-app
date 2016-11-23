d9581084dce4e5e33799895422223445b6b9c73c
EEvvee <dingyiming_sdu@163.com>
2016-11-23 10:48:30 +0800

    xx

--- www/js/app.js
+++ www/js/app.js
@@ -838,7 +838,7 @@ angular.module('starter', ['ionic', 'ngCordova','ngBaiduMap','ionic-datepicker',
       var ob={
         local:function(){
           if(window.cordova!==undefined&&window.cordova!==null)
-            return "http://192.168.3.2:3000";
+            return "http://139.129.96.231:3000";
           else
             return "/proxy/node_server";
             
