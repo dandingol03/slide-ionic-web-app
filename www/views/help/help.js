951e1ff45302e73302d5867c04bacf41a303fb9a
dingyiming <609862262@qq.com>
2017-01-12 14:20:40 +0800

    xx

--- www/views/help/help.js
+++ www/views/help/help.js
@@ -12,10 +12,33 @@ angular.module('starter')
         $scope.go_back=function(){
             window.history.back();
         };
-
         $scope.go_to=function(state){
             $state.go(state);
         };
+
+        /*** 引导页模态框 ***/
+        $ionicModal.fromTemplateUrl('views/modal/help_modal.html',{
+            scope:  $scope,
+            animation: 'animated '+'bounceInUp',
+            hideDelay:920
+        }).then(function(modal) {
+            $scope.help_modal = modal;
+        });
+
+        $scope.openHelpModal= function(){
+            try{
+                $scope.help_modal.show();
+            }catch(e){
+                alert('error=\r\n'+ e.toString());
+            }
+        };
+
+        $scope.closeHelpModal= function() {
+            $scope.help_modal.hide();
+        };
+        /*** 绑定新车模态框 ***/
+
+
 
 
 
